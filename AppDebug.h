54f6f1c4a12aee4423be96cbfd12efccd196d531
Dieter Niklaus <dieter.niklaus@gmx.net>
2017-03-24 19:45:48 +0100

    AppDebug.cpp|.h implementation

---
+++ AppDebug.h
@@ -0,0 +1,16 @@
+/*
+ * AppDebug.h
+ *
+ *  Created on: 24.03.2017
+ *      Author: nid
+ */
+
+#ifndef LIB_APP_DEBUG_APPDEBUG_H_
+#define LIB_APP_DEBUG_APPDEBUG_H_
+
+void setupDebugEnv();
+void dbgCliExecute();
+void sayHello();
+void unrecognized(const char *command);
+
+#endif /* LIB_APP_DEBUG_APPDEBUG_H_ */
